8264be44f0a1a555f87739ebf81ddd9c4248a212
Michael Solovyev <eidolonzx@gmail.com>
2019-03-25 21:51:45 +0400

    Added bin

---
+++ README.md
@@ -0,0 +1,4 @@
+Compares two configuration files and shows a difference.
+
+Step 1
+https://asciinema.org/a/GGetzSEAe9NtMVzj4WEm0vpPC
--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,11 +4,12 @@ import commander from 'commander';
 const program = commander;
 
 program
-    .arguments('<firstConfig> <secondConfig>')
+    .version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
-    .option('-h, --help', 'output usage information')
-    .option('-v, --version', 'output usage information')
+    // .option('-h, --help', 'output usage information')
+    // .option('-v, --version', 'output usage information')
     .option('-f, --format [type]', 'Output format')
+    .arguments('<firstConfig> <secondConfig>')
     .parse(process.argv)
 
 console.log('app is running');
